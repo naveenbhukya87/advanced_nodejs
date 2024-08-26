6dd647a9b8f2738843a2f65ab0dd148be25b1ffd
GOvind MADdala <99980734+govindmaddala@users.noreply.github.com>
2024-08-26 23:15:55 +0530

    synch asynch is added

---
+++ SyncAsynch/SynchAsync.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const Synch = () => {
+    const data = fs.readFileSync("../basics/fs_classes/data.txt", "utf-8")
+    console.log(data)
+    console.log("================================")
+    console.log("Done")
+}
+const Asynch = () => {
+    fs.readFile("../basics/fs_classes/data.txt", "utf-8", (err, data) => {
+        console.log(data)
+    })
+    console.log("================================")
+    console.log("Done")
+}
+
+// Synch()
+Asynch()
+
